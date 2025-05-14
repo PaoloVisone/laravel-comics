36b7ad819e62feb01b5eba53099f486f289f6b74
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-14 18:01:07 +0200

    feat: create card component and integrate it into the home view for dynamic content display

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,6 @@
+<div class="card bg-dark text-white border-0 m-3 shadow-sm" style="width: 12rem;">
+  <img src="{{ $thumb }}" class="card-img-top" alt="{{ $title }}" style="height: 200px; object-fit: cover;">
+  <div class="card-body text-center">
+    <h5 class="card-title">{{ $title }}</h5>
+  </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,38 @@
 @extends('layouts.master')
+
+@php
+    $cards = config('comics')
+@endphp
+
+@section("main")
+<main>
+    <div class="hero">
+        <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron" style="object-position: top; height: 500px; object-fit: cover; width: 100%">
+    </div>
+
+    <div class="container-fluid bg-dark text-white py-4">
+
+
+        <div class="row row-cols-6 justify-content-center mx-5">
+
+            {{-- Cards --}}
+            @foreach ($cards as $card)
+            <x-card>
+
+                <x-slot:title>
+                    {{ $card['title'] }}
+                </x-slot>
+
+                <x-slot:thumb>
+                    {{ $card['thumb'] }}
+                </x-slot>
+                
+            </x-card>
+            @endforeach
+
+            <button type="button" class="btn btn-primary">LOAD MORE</button>
+            
+        </div>
+    </div>
+</main>
+@endsection
