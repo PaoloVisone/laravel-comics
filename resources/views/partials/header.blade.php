ba1907c6ddee08019fe92487b37a1543b570287a
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-14 13:57:40 +0200

    Add header partial with navigation menu

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <div class="nav-bar">
+            <div class="nav-img">
+                <img src="" alt="">
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li><a href=""></a>CHARACTER</li>
+                    <li><a href=""></a>COMICS</li>
+                    <li><a href=""></a>MOVIES</li>
+                    <li><a href=""></a>TV</li>
+                    <li><a href=""></a>GAMES</li>
+                    <li><a href=""></a>COLLECTIBLES</li>
+                    <li><a href=""></a>VIDEOS</li>
+                    <li><a href=""></a>FANS</li>
+                    <li><a href=""></a>NEWS</li>
+                    <li><a href=""></a>SHOP</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
