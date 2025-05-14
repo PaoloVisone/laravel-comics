7c94e3d015b19beecd16b91a2af17f3ddb4ae233
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-14 13:57:52 +0200

    Add footer partial with navigation buttons

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="nav-footer">
+        <div class="nav-button">
+            <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="follow-us">
+        </div>
+    </div>
+</footer>
