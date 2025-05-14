f1be8cdf684d00ddd1109f817f4a22d6b1f5c680
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-14 18:00:04 +0200

    feat: add Font Awesome for enhanced icon support and update footer with social media links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,17 @@
 <footer>
-    <div class="nav-footer">
-        <div class="nav-button">
-            <button>SIGN-UP NOW!</button>
-        </div>
-        <div class="follow-us">
+    <nav class="navbar navbar-expand-lg px-5 bg-dark">
+        <div class="container-fluid ">
+            <a href="#" class="btn btn-outline-primary text-light" tabindex="-1" role="button" >SIGN-UP NOW!</a>        
+        <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link text-primary" href="#">FOLLOW US</a></li>
+                    <li class="nav-item"><a class="nav-link text-secondary" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li class="nav-item"><a class="nav-link text-secondary" href="#"><i class="fa-brands fa-twitter"></i></i></a></li>
+                    <li class="nav-item"><a class="nav-link text-secondary" href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                    <li class="nav-item"><a class="nav-link text-secondary" href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+                    <li class="nav-item"><a class="nav-link text-secondary" href="#"><i class="fa-solid fa-location-dot"></i></a></li>
+                </ul>
+            
         </div>
     </div>
 </footer>
