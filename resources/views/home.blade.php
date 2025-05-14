b272da60eb42d2f93d4707321a9bd712a56abb81
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-14 23:42:50 +0200

    refactor: improve jumbotron layout and adjust container padding for better spacing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,17 @@
 
 @section("main")
 <main>
-    <div class="hero">
-        <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron" style="object-position: top; height: 500px; object-fit: cover; width: 100%">
+    {{-- Jumbotron --}}
+    <div  class="position-relative">
+        <img class="" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron" style="object-position: top; height: 500px; object-fit: cover; width: 100%">
+        <div class="position-absolute top-100 start-50 bg-primary px-3 py-2 text-white" style="z-index: 1; transform: translateX(-660px) translateY(-50%);">
+            CURRENT SERIES
+        </div>
     </div>
 
-    <div class="container-fluid bg-dark text-white py-4">
+    <div class="container-fluid bg-dark text-white py-5">
 
-
-        <div class="row row-cols-6 justify-content-center mx-5">
+        <div class="row row-cols-6 justify-content-center">
 
             {{-- Cards --}}
             @foreach ($cards as $card)
