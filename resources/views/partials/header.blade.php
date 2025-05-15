efbd9978c30ddc2fd8c69f795e3ed4e9b5b54a87
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-15 20:08:50 +0200

    fix: update header links to use named routes for better navigation

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,19 @@
 <header>
+    {{-- Navbar --}}
     <nav class="navbar navbar-expand-lg bg-body-tertiary px-5">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{ route("home") }}">
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            {{-- Menu --}}
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto active">
+                    <li class="nav-item"><a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route("home") }}">COMICS</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->routeIs('shop') ? 'active' : '' }}" href="{{ route("shop") }}">SHOP</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">CHARACTER</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">COMICS</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">MOVIES</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">TV</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">GAMES</a></li>
@@ -18,7 +21,6 @@
                     <li class="nav-item"><a class="nav-link" href="#">VIDEOS</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">FANS</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">NEWS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">SHOP</a></li>
                 </ul>
             </div>
         </div>
