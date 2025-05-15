9d60a182bbc3368ff7677a772484f1249b6e6e09
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-15 20:08:58 +0200

    refactor: replace buy-comics section with component for improved structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,12 @@
+{{-- Layout --}}
 @extends('layouts.master')
 
+{{-- Importo il file con lista di tutti i fumetti  --}}
 @php
     $cards = config('comics')
 @endphp
 
+{{-- Contenuto della homepage --}}
 @section("main")
 <main>
     {{-- Jumbotron --}}
@@ -39,16 +42,7 @@
     </div>
 
     {{-- BUY-COMICS --}}
-    <div class="container-fluid bg-primary p-5 text-center">
-        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital"> 
-        <span class="text-light">DIGITAL COMICS</span>
-        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="merchandise"> 
-        <span class="text-light">DC MERCHANDISE</span>
-        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscriptions"> <span class="text-light">SUBSCRIPTIONS</span>
-        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="locator"> 
-        <span class="text-light">COMIC SHOP LOCATOR</span>
-        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="power"> 
-        <span class="text-light">DC POWER VISA</span>
-    </div>
+    <x-buy>
+    </x-buy>
 </main>
 @endsection
