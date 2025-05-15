5b0c0a5c57df9752e37d9f64b706a2dc32d4875b
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-15 20:08:00 +0200

    feat: add buy-comics component for showcasing comic-related products

---
+++ resources/views/components/buy.blade.php
@@ -0,0 +1,12 @@
+{{-- BUY-COMICS --}}
+    <div class="container-fluid bg-primary p-5 text-center">
+        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital"> 
+        <span class="text-light">DIGITAL COMICS</span>
+        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="merchandise"> 
+        <span class="text-light">DC MERCHANDISE</span>
+        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscriptions"> <span class="text-light">SUBSCRIPTIONS</span>
+        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="locator"> 
+        <span class="text-light">COMIC SHOP LOCATOR</span>
+        <img class="ms-5" style="width: 3em;" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="power"> 
+        <span class="text-light">DC POWER VISA</span>
+    </div>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,5 +3,8 @@
   <img src="{{ $thumb }}" class="card-img-top" alt="{{ $title }}" style="height: 200px; object-fit: cover;">
   <div class="card-body text-center">
     <h5 class="card-title">{{ $title }}</h5>
+     @isset($price)
+    <h6 class="card-subtitle">{{ $price }}</h6>
+    @endisset
   </div>
 </div>
